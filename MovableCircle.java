e77103908bbd28ae3cfc3c21b34315047f5b07da
Artyom <60854544+0reIIIek@users.noreply.github.com>
2020-09-15 22:03:27 +0300

    Add files via upload

---
+++ MovableCircle.java
@@ -0,0 +1,32 @@
+package com.company.ex_4;
+import java.lang.*;
+
+public class MovableCircle implements Movable {
+    int radius;
+    MovablePoint center;
+
+    public MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius) {
+        center = new MovablePoint(x, y, xSpeed, ySpeed);
+        this.radius = radius;
+    }
+    @Override
+    public String toString(){
+        return "Center: "+this.center+", x: "+center.x+", y: "+center.y;
+    }
+    @Override
+    public void moveUp() {
+        center.moveUp();
+    }
+    @Override
+    public void moveDown() {
+        center.moveDown();
+    }
+    @Override
+    public void moveLeft() {
+        center.moveLeft();
+    }
+    @Override
+    public void moveRight() {
+        center.moveRight();
+    }
+}
---
+++ MovableRectangle.java
@@ -0,0 +1,48 @@
+package com.company.ex_4;
+import java.lang.*;
+
+public class MovableRectangle implements Movable {
+    MovablePoint topLeft;
+    MovablePoint bottomRight;
+    public MovableRectangle(int x1, int y1, int x2, int y2, int xSpeed, int ySpeed){
+        topLeft = new MovablePoint(x1,y1,xSpeed,ySpeed);
+        bottomRight = new MovablePoint(x2,y2,xSpeed,ySpeed);
+    }
+    protected boolean isCorrectCoordinates(){
+        return topLeft.xSpeed == bottomRight.xSpeed &&
+                topLeft.ySpeed == bottomRight.ySpeed;
+    }
+    @Override
+    public String toString(){
+        return "Rectangle's coordinates: \ntopLeft: "+topLeft.toString()+
+                "\nbottomRight: "+bottomRight.toString();
+    }
+    @Override
+    public void moveUp() {
+        if (isCorrectCoordinates()){
+            topLeft.moveUp();
+            bottomRight.moveUp();
+        }
+    }
+    @Override
+    public void moveDown() {
+        if (isCorrectCoordinates()){
+            topLeft.moveDown();
+            bottomRight.moveDown();
+        }
+    }
+    @Override
+    public void moveLeft() {
+        if (isCorrectCoordinates()){
+            topLeft.moveLeft();
+            bottomRight.moveLeft();
+        }
+    }
+    @Override
+    public void moveRight() {
+        if (isCorrectCoordinates()){
+            topLeft.moveRight();
+            bottomRight.moveRight();
+        }
+    }
+}
