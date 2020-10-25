55afa2587f5728581228b5223c757b0242909f0f
Artyom <60854544+0reIIIek@users.noreply.github.com>
2020-10-25 18:57:56 +0300

    Add files via upload

---
+++ BoundedWaitList.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class BoundedWaitList extends WaitList{
+    public int capacity;
+
+    public BoundedWaitList(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    @Override
+    public void add(Object element) {
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ UnfairWaitList.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class UnfairWaitList<E> extends WaitList {
+    public UnfairWaitList(){};
+
+    public Object remove(E element) {
+        return content.remove(element);
+    }
+    public void moveToBack(E element){
+        content.offer(element);
+    }
+}
